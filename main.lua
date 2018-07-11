54916c999c8435f0cadd1df926e8984478299239
Michael Oppong <michaeloppong123@gmail.com>
2018-07-11 17:20:22 -0400

    removed some irelevant files/folders

--- main.lua
+++ main.lua
@@ -2,17 +2,7 @@
  halfW = display.contentWidth*0.5;
  halfH = display.contentHeight*0.5;
 
-local composer = require( "composer" );
-
-
+local composer = require( "composer" ); -- in lua, you need to have this line in the main file, so you can navigate to other pages easily
 
 composer.gotoScene( "menu", {effect = "fade", time= 1000});
-display.setStatusBar( display.HiddenStatusBar ) 
-
-
-
-
-
-
-
-
+display.setStatusBar( display.HiddenStatusBar )
